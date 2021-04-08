d94067c269eae6cd45cc7d8c0f3edc46ce798599
alexmi94 <alexandrecance94@gmail.com>
2021-04-08 16:37:29 +0200

    first commit

---
+++ src/validation_commande.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Technos Prod</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<header>
+    <h1>Technos Prod</h1>
+    <h2>Voici le résultat de votre commande</h2>
+</header>
+<main>
+    <section>
+        <h2>Votre commande du <?php print date("d/m/Y, H:i") ?> </h2>
+        <h3>Détail de votre commande:</h3>
+        <p>Nombre de produits : </p>
+        <ul>
+            <li>tt</li>
+            <li>tt</li>
+            <li>tt</li>
+        </ul>
+        <p>Le total de votre commande est de : </p>
+        <p>Adresse : </p>
+    </section>
+</main>
+</body>
